2a11d37bcc476c90648a8c0bfbcb034a77df5ce9
TTG <ttg@TTGmatoMac-mini.local>
2014-09-03 14:45:36 +0800

    Initial Commit

---
+++ Opengles/LAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LAppDelegate.h
+//  Opengles
+//
+//  Created by TTG on 14-9-3.
+//  Copyright (c) 2014年 cn.ttg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
